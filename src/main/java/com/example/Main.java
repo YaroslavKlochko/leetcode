1ff66de728d5bd4b0c01683790fdf65ba46240cc
Yaro Klochko <yaroklochko@gmail.com>
2020-11-20 10:16:36 +0800

    #2 shuffleString was solved successfully

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,5 +2,11 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        ShuffleString shuffleString = new ShuffleString();
+        String s = "codeleet";
+        int[]indices = new int[]{4,5,6,7,0,2,1,3};
+
+        System.out.println(shuffleString.restoreString(s, indices));
     }
 }
---
+++ src/test/groovy/com/example/ShuffleStringTest.groovy
@@ -0,0 +1,22 @@
+package com.example
+
+import spock.lang.Specification
+
+class ShuffleStringTest extends Specification {
+
+    def "should return shuffled string"() {
+        setup:
+        def shuffleString = new ShuffleString()
+
+        expect:
+        shuffleString.restoreString(s as String, indices as int[]) == result as String
+
+        where:
+        s          | indices                  | result
+        "codeleet" | [4, 5, 6, 7, 0, 2, 1, 3] | "leetcode"
+        "abc"      | [0, 1, 2]                | "abc"
+        "aiohn"    | [3, 1, 4, 2, 0]          | "nihao"
+        "aaiougrt" | [4, 0, 2, 6, 7, 3, 1, 5] | "arigatou"
+        "art"      | [1, 0, 2]                | "rat"
+    }
+}
