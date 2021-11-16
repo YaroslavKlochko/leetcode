3b198e1a12ee4ee4c179c7ac033dccb3d5f8b463
YaroslavKlochko <yaroklochko@gmail.com>
2021-11-15 22:03:19 -0500

    added a new leetcode problem

---
+++ src/main/java/com/example/LinearOrSequentialSearch.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class LinearOrSequentialSearch {
+
+    public static void main(String[] args) {
+        int[]mockArr = {2,3,6,1,3,2,6,4,3,7};
+        System.out.println(linearSearch(mockArr, 7));
+    }
+
+    public static int linearSearch(int[] list, int target) {
+        for (int i = 0; i < list.length; i++) {
+            if(list[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
