c82a9a694cac906dbc435be59884219dab94e625
YaroslavKlochko <yaroklochko@gmail.com>
2021-11-15 22:01:02 -0500

    added a new leetcode problem

---
+++ src/main/java/com/example/FibonacciDpAlgorithm.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class FibonacciDpAlgorithm {
+
+    public int fibonacciNum(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+        return fibonacciNum(n - 1) + fibonacciNum(n - 2);
+    }
+}
