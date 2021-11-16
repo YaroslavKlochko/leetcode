f0cfa4c2e29ec013d75ae9d075339a0cf44e36de
YaroslavKlochko <yaroklochko@gmail.com>
2021-11-15 21:57:06 -0500

    added a new leetcode problem

---
+++ src/main/java/com/example/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+    }
+
+        public int binarySearch(int[] nums, int target) {
+            if (nums.length == 0) {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left <= right) {
+                int midpoint = left + (right - left) / 2;
+                if (nums[midpoint] == target) {
+                    return midpoint;
+                } else if (nums[midpoint] > target) {
+                    right = midpoint - 1;
+                } else {
+                    left = midpoint - 1;
+                }
+            }
+            return -1;
+    }
+}
