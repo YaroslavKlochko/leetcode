fbd281f2a625d417c3ca5c321b618af0d96a84e1
YaroslavKlochko <yaroklochko@gmail.com>
2021-11-15 22:04:07 -0500

    added a new leetcode problem

---
+++ src/main/java/com/example/TopUpDpAlgorithm.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class TopUpDpAlgorithm {
+
+    public int fib(int[] memory, int n) {
+        if (memory[n] == 0) {
+            if (n < 2) {
+                memory[n] = n;
+            } else {
+                int left = fib(memory, n - 1);
+                int right = fib(memory, n - 2);
+                memory[n] = left + right;
+            }
+        }
+        return memory[n];
+    }
+}
