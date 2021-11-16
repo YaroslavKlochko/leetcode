40a4782b3e4102badc2b51d694b7b85432083caf
YaroslavKlochko <yaroklochko@gmail.com>
2021-11-15 22:03:40 -0500

    added a new leetcode problem

---
+++ src/main/java/com/example/SelectionSortAlgorithm.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class SelectionSortAlgorithm {
+
+    public static void selectionAlgorithm(int[] list) {
+
+        for (int i = 0; i < list.length - 1; i++) {
+
+            int minIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < list[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+    }
+}
