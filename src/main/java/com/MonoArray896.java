aa98a8613a5438e2dcb7ef52e37a36157e8977aa
Yaroslav Klochko <yaroklochko@gmail.com>
2023-07-27 23:23:44 -0400

    leetcode 896
    Monotonic Array
    An array is monotonic if it is either monotone increasing or monotone decreasing.
    An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
    Given an integer array nums, return true if the given array is monotonic, or false otherwise.

---
+++ src/main/java/com/MonoArray896.java
@@ -0,0 +1,10 @@
+public class MonoArray896 {
+    public boolean isMonotonic(int[] A) {
+        boolean inc = true, dec = true;
+        for (int i = 1; i < A.length; ++i) {
+            inc &= A[i - 1] <= A[i];
+            dec &= A[i - 1] >= A[i];
+        }
+        return inc || dec;
+    }
+}
