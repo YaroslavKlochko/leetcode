ec713fdc61d142413fc898204123b312dd793238
Yaroslav Klochko <yaroklochko@gmail.com>
2023-11-22 11:11:12 -0500

    88. Merge Sorted Array
    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.

---
+++ src/main/java/com/python/merge_sorted_array.py
@@ -0,0 +1,19 @@
+def merge_with_sort(self, nums1, m, nums2, n):
+    for j in range(n):
+        nums1[m + j] = nums2[j]
+    nums1.sort()
+
+def merge (self, nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
