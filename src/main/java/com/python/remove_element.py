356ecc4d9cc1d82c85fe625863592c41a93e8594
Yaroslav Klochko <yaroklochko@gmail.com>
2023-11-23 19:08:29 -0500

    28. Find the Index of the First Occurrence in a String
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

---
+++ src/main/java/com/python/remove_element.py
@@ -0,0 +1,7 @@
+def remove_element(self, nums: List[int], val: int) -> int:
+    index = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index
