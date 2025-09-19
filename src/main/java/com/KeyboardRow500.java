31ae34954c125abfb60e2dbe2ef518c1441d9e7d
Yaroslav Klochko <yaroslavklochko@yaroslavs-mbp.home>
2025-09-18 23:19:50 -0400

    500.Keyboard row

---
+++ src/main/java/com/KeyboardRow500.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class KeyboardRow500 {
+    public String[] findWords(String[] words) {
+        String s1 = "qwertyuiopQWERTYUIOP";
+        String s2 = "asdfghjklASDFGHJKL";
+        String s3 = "zxcvbnmZXCVBNM";
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            int count1=0,count2=0,count3=0,len=words[i].length();
+            for(int j=0;j<len;j++){
+                if(s1.contains(Character.toString(words[i].charAt(j))))
+                    count1++;
+                else if(s2.contains(Character.toString(words[i].charAt(j))))
+                    count2++;
+                else if(s3.contains(Character.toString(words[i].charAt(j))))
+                    count3++;
+                if(count1==len || count2==len || count3==len)
+                    list.add(words[i]);
+            }
+        }
+        String ans[] = new String[list.size()];
+        list.toArray(ans);
+        return ans;
+    }
+}
+
