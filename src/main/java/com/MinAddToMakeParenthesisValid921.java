b0e8af82875f4b11d3a4ede056124852f2b1c7d4
Yaroslav Klochko <yaroklochko@gmail.com>
2026-03-26 18:22:38 -0400

    921: Minimum Add to Make Parentheses Valid

---
+++ src/main/java/com/MinAddToMakeParenthesisValid921.java
@@ -0,0 +1,23 @@
+
+public class MinAddToMakeParenthesisValid921 {
+    public int bracketMatch(String s) {
+        if(s.isEmpty()) {
+            return 0;
+        }
+        int needLeft = 0;
+        int needRight = 0;
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                needRight++;
+            } else if(c == ')') {
+                if(needRight > 0) {
+                    needRight--;
+                } else {
+                    needLeft++;
+                }
+            }
+        }
+         return needLeft + needRight;
+    }
+}
